ed433ee4c3a9a58b69c5cc00f3d06ce0d7007065
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-12-04 19:49:26 -0300

    seletor

---
+++ script.js
@@ -0,0 +1,8 @@
+//com o . se acessa uma propriedade
+// toUpperCase retorna todo o seletor com letra maiúscula
+
+const menu = {
+    seletor: '.principal',
+};
+
+console.log(menu.seletor.toUpperCase());
