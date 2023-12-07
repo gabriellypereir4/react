dfc65786396ec4ff217b122f56a343d85b195624
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-12-07 10:26:52 -0300

    desestruturação de arrays

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 //addEventListener('click') é um evento de clique, então quando eu clicar na tela vai acontecer algo pra mim 
 
-function handleMouse(event){
-    const {clientX, clientY} = event
+function handleMouse({clientX, clientY}){
     console.log(clientX,clientY)
 }
+
+const frutas = ['banana', 'uva']
+const [fruta1, fruta2] = frutas
+console.log(fruta2)
 
 document.addEventListener('click', handleMouse)
