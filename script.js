9fab395fea7edf086aa137d844cf9ea19cebf73b
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-12-07 10:18:13 -0300

    buscando somente 1 propriedade

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //addEventListener('click') é um evento de clique, então quando eu clicar na tela vai acontecer algo pra mim 
 
 function handleMouse(event){
-    console.log(event)
+    console.log(event.clientX)
 }
 
 document.addEventListener('click', handleMouse)
