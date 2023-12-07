8deb373e197282f848c85cff9f9a6cca0c1b0d6f
GabriellyDiasPereira <gabriellydiaspereira15@gmail.com>
2023-12-07 20:14:47 -0300

    Async / Await

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-fetch('https://ranekapi.origamid.dev/json/api/produto')
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(json);
-  });
+async function fetchProdutos(url) {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  }
+  
+  fetchProdutos('https://ranekapi.origamid.dev/json/api/produto');
+  
